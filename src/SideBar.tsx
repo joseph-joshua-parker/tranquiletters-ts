3cf9cd6e7f4ce9d4b614d9d3be3ce6aeeb6a25bd
joseph-joshua-parker <rukifellth@yahoo.com>
2023-10-27 13:15:07 -0300

    hide patternModel for unimplemented sidebar features, fixed its column sizing

--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -14,12 +14,12 @@ const SideBar = () =>{
                 <p className="menu-label">Aspects</p>
                 <ul className="menu-list">
                     <li onClick={()=>handleStimSelect(STIM_TYPES.Token)}><a>Tokens</a></li>
+                    <li onClick={()=>handleStimSelect(STIM_TYPES.Feedback)}><a>Feedback</a></li>
                 </ul>
 
                 <p className="menu-label">Coming Soon...</p>
                 <ul className="menu-list">
                     <li onClick={()=>handleStimSelect(STIM_TYPES.SoundFX)}><a>SoundFX</a></li>
-                    <li onClick={()=>handleStimSelect(STIM_TYPES.Feedback)}><a>Feedback</a></li>
                     <li onClick={()=>handleStimSelect(STIM_TYPES.Ambience)}><a>Ambience</a></li>
                     <li onClick={()=>handleStimSelect(STIM_TYPES.NormalRhythm)}><a>Normal Rhythm</a></li>
                     <li onClick={()=>handleStimSelect(STIM_TYPES.PolyRhythm)}><a>Poly Rhythm</a></li>
--- src/components/StimPatternModel/StimPatternModel.tsx
+++ src/components/StimPatternModel/StimPatternModel.tsx
@@ -1,5 +1,5 @@
 //React & Redux API
-import { batch, useDispatch } from "react-redux";
+import { batch, useDispatch, useSelector } from "react-redux";
 
 //Models & enums
 import { Cursor, PatternUnitModel } from "../../shared/models/patternUnitModel"
@@ -11,6 +11,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 //Redux
 import {  handleTranslate } from "../../state/redux/stimToggleSlice";
+import { RootState } from "../../state/redux/store";
+import { STIM_TYPES } from "../../shared/models/stimTypes";
+
 
 
 interface StimPatternModelProps {
@@ -21,6 +24,10 @@ interface StimPatternModelProps {
 const StimPatternModel: React.FC<StimPatternModelProps> = ({model, cursorIndex})=>{
 
     const dispatch = useDispatch();
+
+    const {currentStimType} = useSelector((state:RootState)=>state.stimToggleSliceReducer)
+    const CurrentlyImplemented = [STIM_TYPES.Token, STIM_TYPES.Feedback]
+    const isFeatureImplemented = CurrentlyImplemented.includes(currentStimType);
 
     const translate = (delta: number)=>{
         batch(()=>{
@@ -36,18 +43,26 @@ const StimPatternModel: React.FC<StimPatternModelProps> = ({model, cursorIndex})
         } 
     )
 
-    return (
-        <div className="columns is-mobile">
-            <div className="column" >
-                <FontAwesomeIcon onClick={()=>translate(-1)} icon={faArrowLeft}/>
-            </div>
-            <div className="column" >
-                {displayedModel}
-            </div>
-            <div className="column" >
-                <FontAwesomeIcon onClick={()=>translate(1)} icon={faArrowRight}/>
-            </div>
+    const modelPanel =    
+    <div className="columns is-mobile">
+        <div className="column is-1" >
+            <FontAwesomeIcon onClick={()=>translate(-1)} icon={faArrowLeft}/>
         </div>
+        <div className="column is-10" >
+            {displayedModel}
+        </div>
+        <div className="column is-1" >
+            <FontAwesomeIcon onClick={()=>translate(1)} icon={faArrowRight}/>
+        </div>
+    </div>
+
+    const comingSoonMessage = <div>Coming soon...</div>
+
+    return (
+        
+            isFeatureImplemented
+            ? modelPanel
+            : comingSoonMessage
     )
 }
 
