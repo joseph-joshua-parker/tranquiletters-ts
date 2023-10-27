dd2b717b0f03ea4863eb7f24aa12897f3724c5c4
joseph-joshua-parker <rukifellth@yahoo.com>
2023-10-27 19:08:52 -0300

    patched an issue where Feedback would disapear because it wasnt actually in the store, deeper fix needed

--- src/components/StimPatternModel/PatternUnitView.tsx
+++ src/components/StimPatternModel/PatternUnitView.tsx
@@ -1,6 +1,6 @@
 
 //React & Redux API
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 
 //Views & Components
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -11,6 +11,8 @@ import { STIM_TYPES } from "../../shared/models/stimTypes"
 
 //Redux
 import { toggleStim } from "../../state/redux/stimToggleSlice"
+import { addFeedback } from "../../state/redux/feedbackParameterSlice"
+import { RootState } from "../../state/redux/store"
 
 interface StimPatternViewProps {
     unit: PatternUnitModel,
@@ -20,17 +22,24 @@ interface StimPatternViewProps {
 const StimPatternView: React.FC<StimPatternViewProps> = ({unit, index})=>{
 
     const dispatch = useDispatch();
+    const {currentStimType} = useSelector((state:RootState)=>state.stimToggleSliceReducer);
 
 
     const isSilence = (unit:PatternUnitModel)=> unit.type == STIM_TYPES.Silence
-
+    const chooseToggle = (index: number)=>{
+        switch(currentStimType){
+            case STIM_TYPES.Feedback: dispatch(addFeedback(index)); break;
+            case STIM_TYPES.Token: dispatch(toggleStim(index)); break;
+            default: return;
+        }
+    }
 
     return (
         <FontAwesomeIcon 
             style={{width:'5vw'}} 
             color={isSilence(unit) ? 'black' : '#303030' } 
             icon={typeToIconMap[unit.type]} 
-            onClick={()=>dispatch(toggleStim(index))}
+            onClick={()=>chooseToggle(index)}
         />)
 }
 
